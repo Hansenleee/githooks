bb6a939153ac137d44b95fe7c5928eac408f9f69
李滨 <18768129451@163.com>
2018-08-24 11:50:58 +0800

    fix: 测试

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "extends": "standard",
+  "plugins": [
+    "standard",
+    "promise"
+  ],
+  "rules": {
+    "semi": [2]
+  },
+  "globals": {
+    "echo": true,
+    "exec": true,
+    "which": true,
+    "rm": true
+  }
+}
